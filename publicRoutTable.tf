b41396b7daf9ff0538d24a9ec8e9418cda24dd19
Kareem Elkasaby <elkasaby@localhost.localdomain>
2020-04-05 16:57:21 +0200

    rout added to private rout table

--- publicRoutTable.tf
+++ publicRoutTable.tf
@@ -13,6 +13,10 @@ resource "aws_route_table" "pubR" {
 resource "aws_route_table" "priR" {
   vpc_id = "${aws_vpc.main.id}"
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
   tags = {
     Name = "priR"
   }
